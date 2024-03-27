8ac6762578a295462a5420492b27de01e1e9ea79
SiMoG97 <echaaranimohamed@gmail.com>
2024-03-27 19:23:05 +0000

    0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys
+import re
+
+regex = '^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-\s(\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{6}\])\s("GET\s\/projects\/260\sHTTP\/1\.1")\s(200|301|400|401|403|404|405|500)\s(\d+)$'
+
+dataToPrint = {
+    "totalSize": 0,
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+
+def printResults():
+    statusDict = dataToPrint.copy()
+    statusDict.pop("totalSize")
+
+    print(f"File size: {dataToPrint['totalSize']}")
+    for status, val in statusDict.items():
+        if val == 0:
+            continue
+        print(f"{status}: {val}")
+
+
+try:
+    for i, line in enumerate(sys.stdin):
+        match = re.match(regex, line)
+        if match == None:
+            continue
+        status = match.groups()[3]
+        size = int(match.groups()[4])
+        dataToPrint["totalSize"] += size
+        dataToPrint[status] += 1
+
+        if (i+1) % 10 == 0:
+            printResults()
+
+except KeyboardInterrupt:
+    printResults()
