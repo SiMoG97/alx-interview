be092054cc131b3a460056fae3b6198c27ec8c11
SiMoG97 <echaaranimohamed@gmail.com>
2024-05-08 23:48:53 +0100

    fixing pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,17 +16,19 @@ def island_perimeter(grid):
     gridHeight = len(grid)
     for i in range(gridHeight):
         for j in range(gridWidth):
-            if grid[i][j] == 1:
-                #  check top cell
-                if i - 1 == -1 or i - 1 >= 0 and grid[i - 1][j] == 0:
-                    perimeter += 1
-                # check bottom cell
-                if i + 1 == gridHeight or i + 1 < gridHeight and grid[i + 1][j] == 0:
-                    perimeter += 1
-                # check left cell
-                if j - 1 == -1 or j - 1 >= 0 and grid[i][j - 1] == 0:
-                    perimeter += 1
-                # check right cell
-                if j + 1 == gridWidth or j + 1 < gridWidth and grid[i][j + 1] == 0:
-                    perimeter += 1
+            if grid[i][j] != 1:
+                continue
+
+            #  check top cell
+            if i - 1 == -1 or i - 1 >= 0 and grid[i - 1][j] == 0:
+                perimeter += 1
+            # check bottom cell
+            if i+1 == gridHeight or i+1 < gridHeight and grid[i+1][j] == 0:
+                perimeter += 1
+            # check left cell
+            if j - 1 == -1 or j - 1 >= 0 and grid[i][j - 1] == 0:
+                perimeter += 1
+            # check right cell
+            if j + 1 == gridWidth or j + 1 < gridWidth and grid[i][j + 1] == 0:
+                perimeter += 1
     return perimeter
