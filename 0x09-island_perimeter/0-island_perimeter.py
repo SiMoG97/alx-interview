43f3d342e9514bc29dd8bf71f31ffd2972b13658
SiMoG97 <echaaranimohamed@gmail.com>
2024-05-08 18:28:39 +0100

    0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" 0-island_perimeter """
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+
+    Args:
+        grid (int[]): list of lists
+
+    Returns:
+        int: the perimeter of the island
+    """
+    perimeter = 0
+    gridWidth = len(grid[0])
+    gridHeight = len(grid)
+    for i in range(gridHeight):
+        for j in range(gridWidth):
+            if grid[i][j] == 1:
+                #  check top cell
+                if i - 1 >= 0 and grid[i - 1][j] == 0:
+                    perimeter += 1
+                # check bottom cell
+                if i + 1 < gridHeight and grid[i + 1][j] == 0:
+                    perimeter += 1
+                # check left cell
+                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                    perimeter += 1
+                # check right cell
+                if j + 1 < gridWidth and grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
